f81742a826b92d2ffe87e557ccd3599a50f835b1
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 20:46:30 +0700

    Create new instance of Player class

--- src/com/erikriosetiawan/Main.java
+++ src/com/erikriosetiawan/Main.java
@@ -1,8 +1,26 @@
 package com.erikriosetiawan;
 
 public class Main {
-    
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        // Create the new instance of Player
+        Player army = new Player("Master Army", 100);
+        // Create the new instance of Weapon and Armor
+        Weapon armyWeapon = new Weapon("Snipper", 980);
+        Armor armyArmor = new Armor("Armor Branch", 550);
+        // Set the army weapon and armor
+        army.setWeapon(armyWeapon);
+        army.setArmor(armyArmor);
+
+        // Create the new instance of Player
+        Player police = new Player("Super Police", 100);
+        // Set the police weapon and armor
+        police.setWeapon(new Weapon("Glock 19", 450));
+        police.setArmor(new Armor("Body Armor", 580));
+
+        // Display all player information
+        army.display();
+        police.display();
     }
 }
