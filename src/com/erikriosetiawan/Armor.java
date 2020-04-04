c55360c9e761f8e9d0dc940df6ddc06b72972a58
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 20:26:40 +0700

    Add the documentations

--- src/com/erikriosetiawan/Armor.java
+++ src/com/erikriosetiawan/Armor.java
@@ -5,11 +5,19 @@ public class Armor {
     private String name;
     private int power;
 
+    /**
+     * Set the armor name and armor power
+     * @param name
+     * @param power
+     */
     public Armor(String name, int power) {
         this.name = name;
         this.power = power;
     }
 
+    /**
+     * Display the armor information
+     */
     public void display() {
         System.out.println("The armor is " + name + " and the power is " + power);
     }
