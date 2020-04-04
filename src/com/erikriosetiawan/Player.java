868817cc78998f4c055e97dacebfecd5d17b05e4
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-04-04 20:25:29 +0700

    Add Player.java file

---
+++ src/com/erikriosetiawan/Player.java
@@ -0,0 +1,49 @@
+package com.erikriosetiawan;
+
+public class Player {
+
+    private String name;
+    private int health;
+
+    private Weapon weapon;
+    private Armor armor;
+
+    /**
+     * Create player name and player health
+     * 
+     * @param name
+     * @param health
+     */
+    public Player(String name, int health) {
+        this.name = name;
+        this.health = health;
+    }
+
+    /**
+     * Set the player weapon
+     * 
+     * @param weapon
+     */
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+
+    /**
+     * Set the player armor
+     * 
+     * @param armor
+     */
+    public void setArmor(Armor armor) {
+        this.armor = armor;
+    }
+
+    /**
+     * Display the player information
+     */
+    public void display() {
+        System.out.println("\nThe player name is " + name);
+        System.out.println("The player health is " + health + " hp");
+        weapon.display();
+        armor.display();
+    }
+}
