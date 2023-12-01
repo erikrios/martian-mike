08c331d5b8c5621be056bb2deb9c01e527392bd2
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-12-01 21:04:44 +0700

    Updating the player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -14,10 +14,27 @@ func _physics_process(delta: float) -> void:
 		if velocity.y > 500:
 			velocity.y = 500
 		
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
 	
 	var direction := Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite_2d.flip_h = direction == -1
+		
 	velocity.x = direction * speed
 	
 	move_and_slide() 
+
+	update_animations(direction)
+	
+func update_animations(direction: int) -> void:
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite_2d.play("jump")
+		else:
+			animated_sprite_2d.play("fall")
